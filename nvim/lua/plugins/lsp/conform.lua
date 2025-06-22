b5382de350c62d731fbeebcc8b57708b1a3b23f3
panevka <97028404+panevka@users.noreply.github.com>
2025-06-22 13:40:52 +0200

    feat: replace prettierd with prettier for js & ts

--- nvim/lua/plugins/lsp/conform.lua
+++ nvim/lua/plugins/lsp/conform.lua
@@ -38,8 +38,10 @@ return {
 			-- python = { "isort", "black" },
 			--
 			-- You can use 'stop_after_first' to run the first available formatter from the list
-			javascript = { "prettierd", "prettier", stop_after_first = true },
-			typescript = { "prettierd", "prettier", stop_after_first = true },
+			javascript = { "prettier", "prettierd", stop_after_first = true },
+			typescript = { "prettier", "prettierd", stop_after_first = true },
+			javascriptreact = { "prettier", "prettierd", stop_after_first = true },
+			typescriptreact = { "prettier", "prettierd", stop_after_first = true },
 		},
 	},
 }
