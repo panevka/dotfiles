e17f17057b9b1133582ed00faea3fd861d5dca9d
panevka <97028404+panevka@users.noreply.github.com>
2025-06-22 13:42:49 +0200

    feat: configure linting for js & ts using eslint

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -81,6 +81,9 @@ require("lazy").setup({
 	-- Autocompletion
 	require("plugins/lsp/nvim-cmp"),
 
+	-- Linting
+	require("plugins/lsp/nvim-lint"),
+
 	-- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
 	-- place them in the correct locations.
---
+++ nvim/lua/plugins/lsp/nvim-lint.lua
@@ -0,0 +1,32 @@
+-- Use any linter for any language in neovim
+return {
+	"mfussenegger/nvim-lint",
+	event = {
+		"BufReadPre",
+		"BufNewFile",
+		"BufWritePost",
+	},
+
+	config = function()
+		local lint = require("lint")
+		lint.linters_by_ft = {
+			javascript = { "eslint" },
+			typescript = { "eslint" },
+			javascriptreact = { "eslint" },
+			typescriptreact = { "eslint" },
+		}
+
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+		vim.api.nvim_create_autocmd({
+			"BufEnter",
+			"BufWritePost",
+			"InsertLeave",
+		}, {
+			group = lint_augroup,
+			callback = function()
+				lint.try_lint()
+			end,
+		})
+	end,
+}
