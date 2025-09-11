adb96a490d8a4119e1bc2c8a37606536eaf4d669
panevka <97028404+panevka@users.noreply.github.com>
2025-09-11 20:45:28 +0200

    feat(nvim): replace ft plugin usage with custom autocmd to prevent bugs & glitches
    done in accordance with this vid: https://youtu.be/iaetgcEzByY

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -11,3 +11,10 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "java",
+	callback = function(args)
+		require("plugins.lsp.jdtls-setup").setup()
+	end,
+})
