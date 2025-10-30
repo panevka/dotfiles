0689ff8c3bdc664fc3b06622a3cfa2a9d3b30f7e
panevka <97028404+panevka@users.noreply.github.com>
2025-10-30 22:28:07 +0100

    feat(nvim): add keybind to go to variable's type definition

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -104,6 +104,9 @@ return {
 				--  For example, in C this would take you to the header.
 				map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
+				-- Go to definition of a variable's type
+				map("gt", require("telescope.builtin").lsp_type_definitions, "[G]oto [T]ype Definition")
+
 				-- The following two autocommands are used to highlight references of the
 				-- word under your cursor when your cursor rests there for a little while.
 				--    See `:help CursorHold` for information about when this is executed
