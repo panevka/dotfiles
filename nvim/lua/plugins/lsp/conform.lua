7fc04762331581d3343bc4850a4868e53ab4d5a1
panevka <97028404+panevka@users.noreply.github.com>
2025-10-30 21:31:31 +0100

    feat(nvim): add auto formatting fallback for languages without specified formatter

--- nvim/lua/plugins/lsp/conform.lua
+++ nvim/lua/plugins/lsp/conform.lua
@@ -16,22 +16,27 @@ return {
 	},
 	opts = {
 		notify_on_error = false,
-		format_on_save = function(bufnr)
-			-- Disable "format_on_save lsp_fallback" for languages that don't
-			-- have a well standardized coding style. You can add additional
-			-- languages here or re-enable it for the disabled ones.
-			-- local disable_filetypes = { cpp = true }
-			local lsp_format_opt
-			-- if disable_filetypes[vim.bo[bufnr].filetype] then
-			-- 	lsp_format_opt = "never"
-			-- else
-			-- 	lsp_format_opt = "fallback"
-			-- end
-			return {
-				timeout_ms = 1000,
-				lsp_format = lsp_format_opt,
-			}
-		end,
+		-- format_on_save = function(bufnr)
+		-- 	-- Disable "format_on_save lsp_fallback" for languages that don't
+		-- 	-- have a well standardized coding style. You can add additional
+		-- 	-- languages here or re-enable it for the disabled ones.
+		-- 	-- local disable_filetypes = { cpp = true }
+		-- 	local lsp_format_opt
+		-- 	-- if disable_filetypes[vim.bo[bufnr].filetype] then
+		-- 	-- 	lsp_format_opt = "never"
+		-- 	-- else
+		-- 	-- 	lsp_format_opt = "fallback"
+		-- 	-- end
+		-- 	return {
+		-- 		timeout_ms = 1000,
+		-- 		lsp_format = lsp_format_opt,
+		-- 	}
+		-- end,
+		format_on_save = {
+			timeout_ms = 1000,
+			lsp_format = "fallback",
+			async = true,
+		},
 		formatters_by_ft = {
 			lua = { "stylua" },
 			-- Conform can also run multiple formatters sequentially
