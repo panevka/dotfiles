f3ee4bc58540684d52fd94d8d9c8570de73390a4
panevka <97028404+panevka@users.noreply.github.com>
2025-11-15 22:53:00 +0100

    feat: add emacs config

---
+++ .emacs.d/init.el
@@ -0,0 +1,10 @@
+
+;; Test if config is loaded
+(message "Emacs config loaded from ~/dev/dotfiles/.emacs.d!")
+
+;; Disable startup splash screen
+(setq inhibit-startup-message t)
+
+;; Enable line numbers globally
+(global-display-line-numbers-mode t)
+
